0f57097e058aa22505883430854025bc17522b2d
Shant Wanes <swanes@bjsrestaurants.com>
2017-10-15 19:26:19 -0700

    DONE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,33 +39,79 @@
         }
 
         function initGame(stat){
-
+          totScore = 0;
           initCrystalValues();
+          randomNumber =0;
           for (var i = 0; i < 4; i++) {
-              $("#img" + (i+1)).attr("data-value", crystalValuesArr[i]);
-             // $("#buttons").append(letterBtn);
+             $("#img" + (i+1)).attr("data-value", crystalValuesArr[i]);
           }
           randomNumber = getRandomNumber(19,120);
-          $("#randomNumber").html(randomNumber);
+          if (status === 'new'){
+              wins =0;
+              losses =0;
+              //$("#randomNumber").html(randomNumber);
+          }
+          status = 'new';
+          refreshDisplay(); 
         }
 
-        function write() {
-          console.log(wins);
-          console.log(losses);
-          console.log(totScore);
-          console.log(crystalValuesArr);
-          console.log(status);
-          console.log(randomNumber);
-          console.log($("#img1").attr("data-value"));
-          console.log($("#img2").attr("data-value"));
-          console.log($("#img3").attr("data-value"));
-          console.log($("#img4").attr("data-value"));
+        function refreshDisplay(){
+           $("#randomNumber").html(randomNumber);
+           $("#totalScore").html("Your Total Score is: " + totScore);
+           $("#wins").html("Wins: " + wins);
+           $("#losses").html("Losses: " + losses);
         }
 
+ //       function write() {
+ //         console.log(wins);
+ //         console.log(losses);
+ //         console.log(totScore);
+ //         console.log(crystalValuesArr);
+ //         console.log(status);
+ //         console.log(randomNumber);
+ //         console.log($("#img1").attr("data-value"));
+ //         console.log($("#img2").attr("data-value"));
+ //         console.log($("#img3").attr("data-value"));
+ //         console.log($("#img4").attr("data-value"));
+ //       }
 
 $(document).ready(function() {
 
-    initGame(status);
-    write();
 
-});
+
+  $(".imgCrystals").on("click", function() {
+        if (status==='new') {
+            totScore = totScore + parseInt($(this).attr("data-value"));
+            $("#totalScore").html("Your Total Score is: " + totScore);
+            
+            if (match(totScore,randomNumber)){
+                addWins();
+                status = 'resume';
+            }
+            else if (totScore > randomNumber){
+              addLosses();
+              status = 'resume';
+            }
+            refreshDisplay();
+            
+      }
+
+    });
+
+  $("#btnNewGame").on("click", function() {
+     status ='new';
+     initGame(status);
+  });
+
+  $("#btnResume").on("click", function() {
+    if (status==='resume'){
+      initGame(status);
+    }
+  });
+
+  $("#btnDisplay").on("click", function() {
+     write();
+  });
+
+
+  });
